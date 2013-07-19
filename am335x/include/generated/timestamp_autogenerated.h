1352fc4127b2e1173e2477c43bcc2ab95484baec
Roman Borisov <ruinrobo@runnlw1019.global.ad.teleca.com>
2013-07-19 12:09:22 +0400

    initial version

---
+++ am335x/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Jul 19 2013"
+#define U_BOOT_TIME "11:58:36"
---
+++ am335x/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2013.07-rc3-00009-gbaa8841-dirty"
+#define U_BOOT_VERSION "U-Boot 2013.07-rc3-00009-gbaa8841-dirty"
+#define CC_VERSION_STRING "arm-linux-gnueabihf-gcc (Ubuntu/Linaro 4.4.7-1ubuntu2) 4.4.7"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils for Ubuntu) 2.22"
